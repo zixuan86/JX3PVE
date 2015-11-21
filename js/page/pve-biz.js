c24dfa7f78fd2b03d14dec49fecf3b5d4b713dfa
17229398@qq.com <17229398@qq.com>
2015-11-21 21:17:45 +0800

    biz sidebar template

--- js/page/pve-biz.js
+++ js/page/pve-biz.js
@@ -1,6 +1,20 @@
 H.ready(['jquery'], function() {
 	jQuery(function($) {
 
+
+		//判断交易状态涂色
+		$("#m-biz-list li a .biz-status").each(function(){
+			var status = $(this).text()
+			if(status == '出售中'){
+				$(this).addClass('onsale')
+			}else if(status == '求购中'){
+				$(this).addClass('onbuy')
+			}else{
+				$(this).addClass('exp')
+			}
+		})
+
+
 		//导航栏激活
 		H.curpage('.u-biz')
 
